81e907013a6963691be880dfad140c927eae1e73
Byeonghoon Han <thehimen9770@gmail.com>
2016-05-26 17:13:38 +0900

    (Initial + Base code) commit

---
+++ app/src/main/java/byeonghoon/x579/smartlock/cardapp/AccountStorage.java
@@ -0,0 +1,7 @@
+package byeonghoon.x579.smartlock.cardapp;
+
+/**
+ * Created by Byeonghoon on 26-May-16.
+ */
+public class AccountStorage {
+}
---
+++ app/src/main/java/byeonghoon/x579/smartlock/cardapp/CardService.java
@@ -0,0 +1,45 @@
+package byeonghoon.x579.smartlock.cardapp;
+
+
+import android.nfc.cardemulation.HostApduService;
+import android.os.Bundle;
+
+public class CardService extends HostApduService {
+
+    public CardService() {
+    }
+
+    @Override
+    public void onDeactivated(int reason) {}
+
+    @Override
+    public byte[] processCommandApdu(byte[] commandApdu, Bundle extras) {
+        return HexStringToByteArray("0000");
+    }
+
+    public static byte[] HexStringToByteArray(String s) throws IllegalArgumentException {
+        int len = s.length();
+        if (len % 2 == 1) {
+            throw new IllegalArgumentException("Hex string must have even number of characters");
+        }
+        byte[] data = new byte[len / 2]; // Allocate 1 byte per 2 hex characters
+        for (int i = 0; i < len; i += 2) {
+            // Convert each character into a integer (base-16), then bit-shift into place
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
+
+    public static String ByteArrayToHexString(byte[] bytes) {
+        final char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char[] hexChars = new char[bytes.length * 2]; // Each byte has two hex characters (nibbles)
+        int v;
+        for (int j = 0; j < bytes.length; j++) {
+            v = bytes[j] & 0xFF; // Cast bytes[j] to int, treating as unsigned value
+            hexChars[j * 2] = hexArray[v >>> 4]; // Select hex character from upper nibble
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F]; // Select hex character from lower nibble
+        }
+        return new String(hexChars);
+    }
+}
